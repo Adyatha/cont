9c765710f6156f4d8f65e9216f79272195eabc04
Adyatha G U <82942928+Adyatha@users.noreply.github.com>
2022-01-22 13:23:36 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,55 +1,4 @@
-<?php
-// if(isset($_POST['name']) || isset($_POST['email']) || isset($_POST['message'])){
-    // set connection varaiables
-$server="localhost";
-$username="root";
-$password="";
-$db="internship";
-// creating connection to database
-$con =  mysqli_connect($server, $username, $password,$db);
-//  checking connection success
-if (!$con) {
-   die("Connection  to this database failed due to ". mysqli_connect_error());
- }
-// collect post variables
-if(isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message =$_POST['message'];
-   
-    $id=10;
-    
-$to='1rn19cs008.adyatha@gmail.com';
-$subject='Form Submission';
-$msg="Name:".$name."\n"."Wrote the following: "."\n\n".$message;
-$headers="From:".$email;
 
-if(mail($to,$subject,$message,$headers)){
-    echo "<h1>Sent Successfully! Thank you"." ".$name.",We will contact you Shortly!";
-}
-}
- 
-
-
-$sql ="INSERT INTO `contact_us`(`name`,`email`,`message`,`time_stamp`) 
-Values ('$name', '$email','$message',current_timestamp());";
-// execute the query
-if($con->query($sql) == true){
-  echo " <script > 
-                alert('thank you! We'll get back to you soon'); 
-                window.location.href = 'index.php';
-                </script>;";
-}
-else{
-  echo "some error occured";
-  echo $con->error();
-}
-
-
-// closing of connection
-$con->close();
-// }
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -90,18 +39,18 @@ $con->close();
                 <br>
                 <div class=bb>
                     <i class="far fa-user" color="gray"></i>&nbsp;&nbsp;
-                    <input  type="text" name="name" placeholder="Name*">
+                    <input  type="text" id=name name="name" placeholder="Name*">
                 </div>
                 
                 <br>
                 <div class=bb>
                     <i class="far fa-envelope"></i>
-                    <input  type="text" name="email" placeholder="Email*">
+                    <input  type="text" id=email name="email" placeholder="Email*">
                 </div>
                 <br>
-                <textarea class=bb rows="10" cols="30" name="message" placeholder="Enter message"></textarea>
+                <textarea id=message class=bb rows="10" cols="30" name="message" placeholder="Enter message"></textarea>
                 <br>
-                <button class=aa id=submit type="submit" value="send message">submit</button><br><br>
+                <button class=aa id=submit type="submit" onClick="sendContact()" value="send message">submit</button><br><br>
                 <div>
                     <i class="fab fa-facebook"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <i class="fab fa-twitter"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -110,41 +59,44 @@ $con->close();
                 </div>
 
             </form>
-            <script>
-        $(document).ready(function() {
+         <script>
+               $(document).ready(function() {
  
-            $("#submit").click(function() {
- 
-                var name = $("#name").val();
-                var email = $("#email").val();
-                var message = $("#message").val();
- 
-                if(name==''||email==''||message=='') {
-                    alert("Please fill all fields.");
-                    return false;
-                }
- 
-                $.ajax({
-                    type: "POST",
-                    url: "store.php",
-                    data: {
-                        name: name,
-                        email: email,
-                        message: message
-                    },
-                    cache: false,
-                    success: function(data) {
-                        alert(data);
-                    },
-                    error: function(xhr, status, error) {
-                        console.error(xhr);
-                    }
-                });
-                 
-            });
- 
-        });
-    </script>
+ $("#submit").click(function() {
+
+     var name = $("#name").val();
+     var email = $("#email").val();
+     var message = $("#message").val();
+
+     if(name==''||email==''||message=='') {
+         alert("Please fill all fields.");
+         return false;
+     }
+     else 
+        alert("Thank you for Submitting");
+     
+
+     $.ajax({
+         type: "POST",
+         url: "config.php",
+         data: {
+             name: name,
+             email: email,
+             message: message
+         },
+         cache: false,
+         success: function(data) {
+             alert(data);
+         },
+         error: function(xhr, status, error) {
+             console.error(xhr);
+         }
+     });
+      
+ });
+
+});
+        </script>
 
         </div>
     </div>
